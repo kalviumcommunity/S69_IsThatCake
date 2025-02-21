ec8f53ffc6108b457b5f1cb8aebe3e5d9f27358d
kalvium <kalvium@kalvium.community>
2025-02-21 12:50:27 +0530

    component done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,33 @@
 // client/src/App.jsx
 import React from 'react';
 import './App.css';
+import UserCard from './components/CakeCard';
+
+
 
 function App() {
+  // Dummy data for the cake
+  const cakeData = {
+    image: "https://preview.redd.it/o6wk4jkf64961.jpg?width=640&crop=smart&auto=webp&s=52c494257495b8ba9ccd5dff84c311114c99f765", // Link to a weird cake image
+    name: "The Moldy Cake",
+    description: "A cake that was left out too long. Don't eat it!",
+    rating: 2,
+    uploader: "CakeLover123"
+  };
+
   return (
     <div className="app">
       <header className="hero">
         <h1>IsThatCake</h1>
-        <p>Weird cakes. funny ones. laughters.</p>
+        <p>Weird cakes. Funny ones. Laughs guaranteed!</p>
         <button>Explore Gallery</button>
       </header>
+
+     
+      <section className="cake-gallery">
+        <UserCard {...cakeData} />
+        
+      </section>
     </div>
   );
 }
